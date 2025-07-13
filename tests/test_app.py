6e89e560e410b8411a2dbdce1eef1d644fa4d2d3
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-07-13 19:12:41 +1000

    printing response data

--- tests/test_app.py
+++ tests/test_app.py
@@ -42,8 +42,14 @@ def test_create_team_member(client):
         "tenure": 1,
         "salary": 50000
     })
+
+    print(response.data)
+
     assert response.status_code == 201
     data = response.get_json()
+    print(data)
+
     assert data['first_name'] == "Test"
     assert data['last_name'] == "User"
     assert data['start_date'] == "2023-01-01"
+
